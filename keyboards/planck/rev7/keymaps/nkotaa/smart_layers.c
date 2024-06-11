6a5d89fa9cd72a30b2130738f59c9aaaf339b7fe
nkotaa <e0957774@u.nus.edu>
2024-06-11 18:21:07 +0800

    Change number switching algorithm to pre-switch

--- keyboards/planck/rev7/keymaps/nkotaa/smart_layers.c
+++ keyboards/planck/rev7/keymaps/nkotaa/smart_layers.c
@@ -3,25 +3,6 @@
 static enum SmartSwitchMode smart_switch_mode = MODE_OFF;
 static bool has_ext_post_elapsed = false;
 
-void pre_process_sm_ext_kc(uint16_t keycode, keyrecord_t *record) {
-    assert(keycode == SM_EXT);
-    if (!record->event.pressed) {
-        return;
-    }
-
-    if (!IS_SMART_LAYER_ON()) {
-        layer_on(_EXTEND);
-    } else {
-        smart_switch_mode = MODE_INVERSE;
-    }
-}
-
-//void smart_ext_off(void) {
-//    has_ext_post_elapsed = false;
-//    set_last_mods(0);
-//    layer_off(_EXTEND);
-//}
-
 bool is_same_cluster(uint16_t current_keycode, uint16_t last_keycode) {
     switch (last_keycode) {
     case KC_RGHT ... KC_LEFT:
@@ -30,7 +11,8 @@ bool is_same_cluster(uint16_t current_keycode, uint16_t last_keycode) {
     case KC_DOWN ... KC_UP:
         return (current_keycode >= KC_RGHT && current_keycode <= KC_UP);
     case KC_1 ... KC_0:
-        return current_keycode >= KC_1 && current_keycode <= KC_0;
+        return (current_keycode >= KC_1 && current_keycode <= KC_0) ||
+            current_keycode == KC_BSPC;
     case KC_TAB:
         return current_keycode == last_keycode;
     case KC_ENT ... KC_BSPC:
@@ -40,54 +22,27 @@ bool is_same_cluster(uint16_t current_keycode, uint16_t last_keycode) {
     }
 }
 
-// needs to be evaluated and executed on both key press and release to avoid lag
-void smart_layer_postlapse(uint16_t keycode, bool has_mods, bool has_last_mods, keyrecord_t *record) {
-    if (smart_switch_mode != MODE_POST) {
-        return;
-    }
-    if (has_ext_post_elapsed && !has_mods) {
-        layer_off(_EXTEND);
-        smart_switch_mode = MODE_OFF;
-        has_ext_post_elapsed = false;
-        return;
-    }
-    bool is_keycode_osm = keycode == OSM_SFT || keycode == OSM_ALT ||
-        keycode == OSM_GUI || keycode == OSM_CTL;
-    has_ext_post_elapsed = has_ext_post_elapsed || (has_last_mods && !is_keycode_osm);
-}
-
-// pay attention to whether to process these keys on press (event.pressed)
-// or release (!event.pressed)
-bool process_record_smart_layer_kc(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-    case KC_1 ... KC_0:
-    case OSM_SFT:
-    case OSM_ALT:
-    case OSM_GUI:
-    case OSM_CTL:
-        if (record->event.pressed) {
-            smart_switch_mode = MODE_POST;
-        }
-        return true;
-    case KC_RGHT ... KC_UP:
-    case KC_ENT ... KC_TAB:
-    case KC_DEL:
-        if (!record->event.pressed && smart_switch_mode != MODE_POST) {
-            smart_switch_mode = MODE_PRE;
-        }
-        return true;
-    default:
-        return false;
-    }
-}
-
 bool is_escape_sequence(uint16_t current_keycode, uint16_t last_keycode) {
-    switch (current_keycode) {
-    case KC_ENT:
-        return last_keycode == KC_DOWN || last_keycode == KC_UP ||
-            last_keycode == KC_TAB;
+    switch (last_keycode) {
+    case KC_DOWN ... KC_UP:
+    case KC_TAB:
+        return current_keycode == KC_ENT;
+    case KC_1 ... KC_0:
+        return current_keycode == KC_UP || current_keycode == KC_DOWN;
     default:
         return false;
+    }
+}
+
+void pre_process_sm_ext_kc(uint16_t keycode, keyrecord_t *record) {
+    assert(keycode == SM_EXT);
+    if (!record->event.pressed) {
+        return;
+    }
+    if (!IS_SMART_LAYER_ON()) {
+        layer_on(_EXTEND);
+    } else {
+        smart_switch_mode = MODE_INVERSE;
     }
 }
 
@@ -105,9 +60,6 @@ void smart_layer_elapse_preroutine(uint16_t current_keycode, uint16_t last_keyco
     case MODE_OFF:
         return;
     case MODE_POST:
-        if (last_keycode >= KC_1 && last_keycode <= KC_0) {
-            has_ext_post_elapsed = has_ext_post_elapsed || !is_from_same_cluster;
-        }
         return;
     case MODE_INVERSE:
         has_ext_elapsed = is_from_same_cluster;
@@ -128,4 +80,45 @@ void smart_layer_elapse_preroutine(uint16_t current_keycode, uint16_t last_keyco
         layer_off(_EXTEND);
         smart_switch_mode = MODE_OFF;
     }
+}
+
+// pay attention to whether to process these keys on press (event.pressed)
+// or release (!event.pressed)
+bool process_record_smart_layer_kc(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case OSM_SFT:
+    case OSM_ALT:
+    case OSM_GUI:
+    case OSM_CTL:
+        if (record->event.pressed) {
+            smart_switch_mode = MODE_POST;
+        }
+        return true;
+    case KC_1 ... KC_0:
+    case KC_RGHT ... KC_UP:
+    case KC_ENT ... KC_TAB:
+    case KC_DEL:
+        if (!record->event.pressed && smart_switch_mode != MODE_POST) {
+            smart_switch_mode = MODE_PRE;
+        }
+        return true;
+    default:
+        return false;
+    }
+}
+
+// needs to be evaluated and executed on both key press and release to avoid lag
+void smart_layer_postlapse(uint16_t keycode, bool has_mods, bool has_last_mods, keyrecord_t *record) {
+    if (smart_switch_mode != MODE_POST) {
+        return;
+    }
+    if (has_ext_post_elapsed && !has_mods) {
+        layer_off(_EXTEND);
+        smart_switch_mode = MODE_OFF;
+        has_ext_post_elapsed = false;
+        return;
+    }
+    bool is_keycode_osm = keycode == OSM_SFT || keycode == OSM_ALT ||
+        keycode == OSM_GUI || keycode == OSM_CTL;
+    has_ext_post_elapsed = has_ext_post_elapsed || (has_last_mods && !is_keycode_osm);
 }
